293dbb7f0a44b2f64c72411f4c75e24771d50ada
ErochiN <se6350235@gmail.com>
2023-10-14 13:16:53 +0300

    Добавлена проверка на условие

--- Binary.cpp
+++ Binary.cpp
@@ -6,216 +6,246 @@
 class Binary
 {
 private:
-    //Число в десятичной системе
-    int decimalSystem;
+	//Число в десятичной системе
+	int decimalSystem;
 
-    //Число в двоичной системе
-    int binarySystem;
+	//Число в двоичной системе
+	int binarySystem;
 
-    //Перевод десятичного числа в двоичное
-    void Bin(int number)
-    {
-        std::string binCode;
+	//Перевод десятичного числа в двоичное
+	void Bin(int number)
+	{
+		std::string binCode;
 
-        while (number != 0)
-        {
-            binCode = std::to_string(number % 2) + binCode;
-            number /= 2;
-        }
-        this->binarySystem = std::stoi(binCode);
-    }
+		while (number != 0)
+		{
+			binCode = std::to_string(number % 2) + binCode;
+			number /= 2;
+		}
+		this->binarySystem = std::stoi(binCode);
+	}
 
 public:
-    //Конструктор по умолчанию
-    Binary()
-    {
-        this->decimalSystem = 0;
-        this->binarySystem = 0;
-    }
+	//Конструктор по умолчанию
+	Binary()
+	{
+		this->decimalSystem = 0;
+		this->binarySystem = 0;
+	}
 
-    //Конструктор иницилизации
-    Binary(int number)
-    {
-        this->decimalSystem = number;
-        this->Bin(this->decimalSystem);
-    }
+	//Конструктор иницилизации
+	Binary(int number)
+	{
+		this->decimalSystem = number;
+		this->Bin(this->decimalSystem);
+	}
 
-    //Присваивание значения int
-    Binary& operator = (int number)
-    {
-        this->decimalSystem = number;
-        Bin(number);
-        return *this;
-    }
+	//Присваивание значения int
+	Binary& operator = (int number)
+	{
+		this->decimalSystem = number;
+		Bin(number);
+		return *this;
+	}
 
-    //Присваивания значения Binary
-    Binary& operator = (const Binary& other)
-    {
-        this->decimalSystem = other.decimalSystem;
-        this->binarySystem = other.binarySystem;
-        return *this;
-    }
+	//Присваивания значения Binary
+	Binary& operator = (const Binary& other)
+	{
+		this->decimalSystem = other.decimalSystem;
+		this->binarySystem = other.binarySystem;
+		return *this;
+	}
 
-    //Сложение бинарных чисел
-    Binary& operator + (const Binary& other)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem + other.decimalSystem;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Сложение бинарных чисел
+	Binary operator + (const Binary& other)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem + other.decimalSystem;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Сложение и присваивание бинарных чисел
-    Binary& operator += (const Binary& other)
-    {
-        int sum = this->decimalSystem + other.decimalSystem;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Сложение и присваивание бинарных чисел
+	Binary& operator += (const Binary& other)
+	{
+		int sum = this->decimalSystem + other.decimalSystem;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Разность бинарных числел
-    Binary& operator - (const Binary& other)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem - other.decimalSystem;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Разность бинарных числел
+	Binary operator - (const Binary& other)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem - other.decimalSystem;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Разность и присваивание бинарных чисел
-    Binary& operator -= (const Binary& other)
-    {
-        int sum = this->decimalSystem - other.decimalSystem;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Разность и присваивание бинарных чисел
+	Binary& operator -= (const Binary& other)
+	{
+		int sum = this->decimalSystem - other.decimalSystem;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Умножение бинарных чисел
-    Binary& operator * (const Binary& other)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem * other.decimalSystem;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Умножение бинарных чисел
+	Binary operator * (const Binary& other)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem * other.decimalSystem;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Умножение и присваивание бинарных чисел
-    Binary& operator *= (const Binary& other)
-    {
-        int sum = this->decimalSystem * other.decimalSystem;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Умножение и присваивание бинарных чисел
+	Binary& operator *= (const Binary& other)
+	{
+		int sum = this->decimalSystem * other.decimalSystem;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Деление бинарных чисел
-    Binary& operator / (const Binary& other)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem / other.decimalSystem;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Деление бинарных чисел
+	Binary operator / (const Binary& other)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem / other.decimalSystem;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Деление и присваивание бинарных чисел
-    Binary& operator /= (const Binary& other)
-    {
-        int sum = this->decimalSystem / other.decimalSystem;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Деление и присваивание бинарных чисел
+	Binary& operator /= (const Binary& other)
+	{
+		int sum = this->decimalSystem / other.decimalSystem;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Сложение Бинарного числа с переменной
-    template <typename T>
-    Binary& operator + (T number) 
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem + number;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Сложение Бинарного числа с переменной
+	template <typename T>
+	Binary operator + (T number)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem + number;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Разность Бинарного числа с переменной
-    template <typename T>
-    Binary& operator - (T number)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem - number;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Разность Бинарного числа с переменной
+	template <typename T>
+	Binary operator - (T number)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem - number;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Умножение Бинарного числа с переменной
-    template <typename T>
-    Binary& operator * (T number)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem * number;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Умножение Бинарного числа с переменной
+	template <typename T>
+	Binary operator * (T number)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem * number;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Диление Бинарного числа с переменной
-    template <typename T>
-    Binary& operator / (T number)
-    {
-        Binary temp;
-        temp.decimalSystem = this->decimalSystem / number;
-        temp.Bin(temp.decimalSystem);
-        return temp;
-    }
+	//Диление Бинарного числа с переменной
+	template <typename T>
+	Binary operator / (T number)
+	{
+		Binary temp;
+		temp.decimalSystem = this->decimalSystem / number;
+		temp.Bin(temp.decimalSystem);
+		return temp;
+	}
 
-    //Сложение и пресваивание переиенной
-    template <typename T>
-    Binary& operator += (T number)
-    {
-        int sum = this->decimalSystem + number;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Сложение и пресваивание переиенной
+	template <typename T>
+	Binary& operator += (T number)
+	{
+		int sum = this->decimalSystem + number;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Разность и пресваивание переиенной
-    template <typename T>
-    Binary& operator -= (T number)
-    {
-        int sum = this->decimalSystem - number;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Разность и пресваивание переиенной
+	template <typename T>
+	Binary& operator -= (T number)
+	{
+		int sum = this->decimalSystem - number;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Сложение и пресваивание переиенной
-    template <typename T>
-    Binary& operator *= (T number)
-    {
-        int sum = this->decimalSystem * number;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Сложение и пресваивание переиенной
+	template <typename T>
+	Binary& operator *= (T number)
+	{
+		int sum = this->decimalSystem * number;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Диление и пресваивание переиенной
-    template <typename T>
-    Binary& operator /= (T number)
-    {
-        int sum = this->decimalSystem / number;
-        this->decimalSystem = sum;
-        this->Bin(sum);
-        return *this;
-    }
+	//Диление и пресваивание переиенной
+	template <typename T>
+	Binary& operator /= (T number)
+	{
+		int sum = this->decimalSystem / number;
+		this->decimalSystem = sum;
+		this->Bin(sum);
+		return *this;
+	}
 
-    //Вывод на экран бинарных чисел
-    void Show()
-    {
+	bool operator == (const Binary& other)
+	{
+		return this->decimalSystem == other.decimalSystem && this->binarySystem == other.binarySystem;
+	}
+
+	bool operator != (const Binary& other)
+	{
+		return !(this->decimalSystem == other.decimalSystem && this->binarySystem == other.binarySystem);
+	}
+
+	bool operator <= (const Binary& other)
+	{
+		return this->decimalSystem <= other.decimalSystem;
+	}
+
+	bool operator >= (const Binary& other)
+	{
+		return this->decimalSystem >= other.decimalSystem;
+	}
+
+	bool operator < (const Binary& other)
+	{
+		return this->decimalSystem < other.decimalSystem;
+	}
+
+	bool operator > (const Binary& other)
+	{
+		return this->decimalSystem > other.decimalSystem;
+	}
+
+	//Вывод на экран бинарных чисел
+	void Show()
+	{
 #ifdef debug
-        std::cout << decimalSystem << std::endl;
+		std::cout << decimalSystem << " " << this << std::endl;
 #endif;
-        std::cout << binarySystem << std::endl;
-    }
+		std::cout << binarySystem << std::endl;
+	}
 
 
 
@@ -223,13 +253,12 @@ public:
 
 int main()
 {
-    Binary a;
+	Binary a = 20;
+	Binary b = 20;
 
-    a = 20;
-    a.Show();
+	bool i = a >= b;
 
-    a += 20;
-    a.Show();
+	std::cout << i;
 
-    return 0;
+	return 0;
 }
